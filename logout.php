f01908b9b79c2ac7232cbe8764d52ca2258b0da6
AntonArmand <armand.anton@limayrac.fr>
2018-03-06 16:26:39 +0100

    readme & logout.php

--- logout.php
+++ logout.php
@@ -2,34 +2,7 @@
 session_start();
 session_unset();
 session_destroy();
-$submit = isset($_POST['submit']) ? $_POST['submit'] : '';
+header('Location: index.php');
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo' <script type="text/javascript">
-            alert("Deconnexion reussite ");
-            </script>';
-        ?>
-    <center>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="form1" method="post">
 
 
-                <p><input type="submit" name="submit" value="OK" /></p>
-
-
-            </form>
-    </center>
-        <?php
-        if($submit)
-        {
-        header('Location: index.php');
-        }
-        ?>
-        </body>
-        </html>
